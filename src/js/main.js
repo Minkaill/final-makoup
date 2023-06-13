b6bb91787ab71ca74e23d051625e2f22725dd891
minkail <mkx.dake@gmail.com>
2023-06-13 17:41:17 +0300

    fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,29 @@
 import "../scss/main.scss";
+import "./brandSwiper";
+import "./showMore";
+
+// картинки
+import burger from "../img/burger.svg";
+import call from "../img/call.svg";
+import chat from "../img/chat.svg";
+import checkstatus from "../img/checkstatus.svg";
+import cps from "../img/cps.png";
+import group from "../img/Group.svg";
+import profile from "../img/profile.svg";
+import repair from "../img/repair.svg";
+import expand from "../img/expand.svg";
+import line from "../img/Line.svg";
+
+// бренды
+import iphone from "../img/brands/iphone.svg";
+import acer from "../img/brands/acer.svg";
+import lenovo from "../img/brands/lenovo.svg";
+import samsung from "../img/brands/samsung.svg";
+import bosch from "../img/brands/bosch.svg";
+import hp from "../img/brands/hp.svg";
+
+// технологии
+import laptop from "../img/tehnology/remont-laptop.svg";
+import tablet from "../img/tehnology/remont-tablet.svg";
+import pc from "../img/tehnology/remont-pc.svg";
+import dt from "../img/tehnology/remont-dt.svg";
